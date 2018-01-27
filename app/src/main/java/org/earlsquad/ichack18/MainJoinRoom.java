165ced6b14e5230820697c8007f3e67201e56109
Terrence Chan <ckc116@ic.ac.uk>
2018-01-27 21:09:45 +0000

    RoomCreation and JoinRoom created

---
+++ app/src/main/java/org/earlsquad/ichack18/MainJoinRoom.java
@@ -0,0 +1,13 @@
+package org.earlsquad.ichack18;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainJoinRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.joinroom);
+    }
+}
