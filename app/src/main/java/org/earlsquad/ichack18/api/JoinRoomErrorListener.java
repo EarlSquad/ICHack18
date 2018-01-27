f091e5319fb814ba108bbb2e4df0921258f6344f
Noel Lee <leekayau@gmail.com>
2018-01-27 21:45:15 +0000

    Added room full listening

---
+++ app/src/main/java/org/earlsquad/ichack18/api/JoinRoomErrorListener.java
@@ -0,0 +1,6 @@
+package org.earlsquad.ichack18.api;
+
+public interface JoinRoomErrorListener {
+  void roomFullError();
+  void joinSuccessful();
+}
--- app/src/main/java/org/earlsquad/ichack18/api/RoomManager.java
+++ app/src/main/java/org/earlsquad/ichack18/api/RoomManager.java
@@ -40,8 +40,44 @@ public class RoomManager {
     });
   }
 
-  public void joinRoom(String roomName, String userName) {
-    DatabaseReference rooms = database.getReference(ROOMS);
-    rooms.child(roomName).child(USERS).push().setValue(userName);
+  public void joinRoom(String roomName, final String userName, final JoinRoomErrorListener listener) {
+    final DatabaseReference room = database.getReference(ROOMS).child(roomName);
+    room.addListenerForSingleValueEvent(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        long userCount = dataSnapshot.getChildrenCount();
+        if (userCount < 4) {
+          room.child(USERS).push().setValue(userName);
+          if (listener != null) listener.joinSuccessful();
+        } else {
+          if (listener != null) listener.roomFullError();
+        }
+      }
+
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+
+      }
+    });
+  }
+
+  public void waitForRoomFull(String roomName, final RoomFullListener listener) {
+    DatabaseReference room = database.getReference(ROOMS).child(roomName);
+
+    room.child("users").addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        long userCount = dataSnapshot.getChildrenCount();
+        if (userCount == 4) {
+          Log.d(TAG, "Room is full");
+          listener.roomFull();
+        }
+      }
+
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+
+      }
+    });
   }
 }
