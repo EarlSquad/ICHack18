9fcfe167581deb84e33a9b1bd8650108b40ecf6a
Terrence Chan <ckc116@ic.ac.uk>
2018-01-28 00:52:55 +0000

    takePic created

--- app/src/main/java/org/earlsquad/ichack18/Room.java
+++ app/src/main/java/org/earlsquad/ichack18/Room.java
@@ -1,7 +1,9 @@
 package org.earlsquad.ichack18;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Room extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Room extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
+    }
+
+    public void goToTakepic(View v){
+        startActivity(new Intent(Room.this, TakePic.class));
     }
 }
