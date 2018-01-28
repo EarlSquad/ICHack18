08e610dce5c3a87d2cfa418fe6d7ade4ae7b9453
Noel Lee <leekayau@gmail.com>
2018-01-28 08:48:16 +0000

    Integrated join room logic to ui

--- app/src/main/java/org/earlsquad/ichack18/MainJoinRoom.java
+++ app/src/main/java/org/earlsquad/ichack18/MainJoinRoom.java
@@ -4,17 +4,43 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import org.earlsquad.ichack18.api.JoinRoomListener;
+import org.earlsquad.ichack18.api.RoomManager;
 
 public class MainJoinRoom extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.joinroom);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.joinroom);
+
+    String roomName = getIntent().getStringExtra("roomName");
+    if (roomName != null) {
+      joinRoom(roomName, getIntent().getStringExtra("userName"));
     }
+  }
 
+  private void joinRoom(String roomName, String userName) {
+    RoomManager.getInstance().joinRoom(roomName, userName, new JoinRoomListener() {
+      @Override
+      public void roomFullError() {
+        Toast.makeText(MainJoinRoom.this, "Room is full :(", Toast.LENGTH_LONG).show();
+      }
 
-    public void goToRoom(View v){
+      @Override
+      public void joinSuccessful(String userId) {
         startActivity(new Intent(MainJoinRoom.this, Room.class));
-    }
+      }
+    });
+  }
+
+  public void goToRoom(View v) {
+    EditText roomNameView = (EditText) findViewById(R.id.join_room_name);
+    EditText userNameView = (EditText) findViewById(R.id.join_user_name);
+    String roomName = roomNameView.getText().toString();
+    String userName = userNameView.getText().toString();
+    joinRoom(roomName, userName);
+  }
 }
--- app/src/main/java/org/earlsquad/ichack18/MainRoomCreation.java
+++ app/src/main/java/org/earlsquad/ichack18/MainRoomCreation.java
@@ -4,19 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+import org.earlsquad.ichack18.api.RoomExistsListener;
+import org.earlsquad.ichack18.api.RoomManager;
 
 public class MainRoomCreation extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.createroom);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.createroom);
+  }
 
-    public void goToJoinRoom(View v){
-        Toast toast = Toast.makeText(getApplicationContext(), "ac", Toast.LENGTH_SHORT);
-        toast.show();
-        startActivity(new Intent(MainRoomCreation.this, MainJoinRoom.class));
-    }
+  public void goToJoinRoom(View v) {
+    EditText createRoomNameView = (EditText) findViewById(R.id.create_room_name);
+    EditText userNameView = (EditText) findViewById(R.id.new_user_name);
+    final String roomName = createRoomNameView.getText().toString();
+    final String userName = userNameView.getText().toString();
+    RoomManager.getInstance().createRoom(roomName, new RoomExistsListener() {
+      @Override
+      public void roomExists() {
+        Toast.makeText(getApplicationContext(), "Room name already exist", Toast.LENGTH_LONG).show();
+      }
+
+      @Override
+      public void roomNotExists() {
+        Intent intent = new Intent(MainRoomCreation.this, MainJoinRoom.class);
+        intent.putExtra("userName", userName);
+        intent.putExtra("roomName", roomName);
+        startActivity(intent);
+      }
+    });
+  }
 }
