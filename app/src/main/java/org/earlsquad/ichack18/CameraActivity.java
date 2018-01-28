709af2832d4e0b36479085da97b2563a380435a5
Noel Lee <leekayau@gmail.com>
2018-01-28 03:44:11 +0000

    Added stuff

--- app/src/main/java/org/earlsquad/ichack18/CameraActivity.java
+++ app/src/main/java/org/earlsquad/ichack18/CameraActivity.java
@@ -4,7 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import org.opencv.android.*;
-import org.opencv.core.Mat;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class CameraActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
   private static final String TAG = "CameraActivity";
@@ -73,6 +79,29 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
 
   @Override
   public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame cvCameraViewFrame) {
-    return cvCameraViewFrame.rgba();
+    Mat original = cvCameraViewFrame.rgba();
+    Mat result = original.clone();
+    Imgproc.adaptiveThreshold(cvCameraViewFrame.gray(), result, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 15, 10);
+    List<MatOfPoint> contours = new ArrayList<>(100);
+    Imgproc.findContours(result, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+    Collections.sort(contours, new Comparator<MatOfPoint>() {
+      @Override
+      public int compare(MatOfPoint mop1, MatOfPoint mop2) {
+        int area1 = (int) (mop1.size().height * mop1.size().width);
+        int area2 = (int) (mop2.size().height * mop2.size().width);
+        if (area1 > area2) {
+          return -1;
+        }
+        if (area1 < area2) {
+          return 1;
+        }
+        return 0;
+      }
+    });
+    MatOfPoint mop = contours.get(0);
+    for (Point point : mop.toArray()) {
+      Imgproc.circle(result, point, 5, new Scalar(255,0,0,1), 2);
+    }
+    return result;
   }
 }
